003a027965f0b5e0fb12ad984eb2ac3c2987240c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-25 02:06:20 +0000

    betty checked & libraries added

--- monty.h
+++ monty.h
@@ -1,8 +1,11 @@
 #ifndef MONTY_H
 #define MONTY_H
 #include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -14,10 +17,11 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcoode and its function
  * @opcode: the opcode
@@ -28,7 +32,7 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 #endif
