3a472e28c6ece3e2dea12e733bf64c1d71af4015
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-27 21:23:30 +0000

    changed atoi function to buildin

--- monty.c
+++ monty.c
@@ -101,7 +101,7 @@ void read_file(const char *file_name)
 		}
 		if (token_1 != NULL)
 		{
-			tokennum = _atoi(token_1);
+			tokennum = atoi(token_1);
 			if ((strcmp(token_0, "push") == 0 && tokennum == 0 && (strcmp(token_1, "0") != 0)))
 			{
                                 printf("L%d: usage: push integer\n", line_num);
@@ -109,7 +109,7 @@ void read_file(const char *file_name)
                                 free(strinput);
                                 exit(EXIT_FAILURE);
 			}
-			}
+		}
 
 		p = getopcode(token_0);
 		if (p == NULL)
