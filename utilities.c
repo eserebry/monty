8840e4621f2506f1098644ada06165ba2664f47c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-27 03:28:45 +0000

    back to old version of itoa

--- utilities.c
+++ utilities.c
@@ -5,7 +5,7 @@
  *
  *Return: converted int
  */
-/*int _atoi(char *number)
+int _atoi(char *number)
 {
 	int value = 0;
 	int boo = 0;
@@ -30,38 +30,8 @@
 	}
 	return (value);
 
-	}*/
-int _atoi(char *s)
-{
-	int i;
-	unsigned int in;
-	int sign;
-
-	i = 0;
-	in = 0;
-	sign = 0;
-	while (s[i] > '9' || s[i] < '0')
-	{
-		if (s[i] == '-')
-		{
-			sign++;
-		}
-		i++;
-	}
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		in = in * 10 + (s[i] - '0');
-		i++;
-	}
-	if (sign % 2 != 0)
-	{
-		in = -1 * in;
-		return (in);
-	}
-	if (in == 0)
-		return (0);
-	return  (in);
 }
+
 /**
  * _free - function frees all memory stored in heap
  *@head: pointer to head of linked list
